585426befaa37238f6911763ce4151b945f999b7
Kritul <kritulrathod@gmail.com>
2017-09-26 01:54:18 -0400

    Initial Checkin - Basic Scythe probe implementation

---
+++ ScytheConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Scythe;
+
+namespace ScytheConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string command = DateTime.Now.ToString();
+            Probe.InitializeScythe();
+
+            while (!string.IsNullOrEmpty(command) && command.ToLower() != "x")
+            {
+                var date = Convert.ToDateTime(command);
+
+                Probe.ScytheProbe("Console_Marker");
+                //Probe.SetMarkTime("Console_Marker",date); //Just for testing. this should be Private method.
+
+                command = Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ScytheTest/ScytheTest.cs
@@ -0,0 +1,46 @@
+﻿using Scythe;
+using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScytheTest
+{
+    [TestClass]
+    public class ScytheTest
+    {
+        [TestMethod]
+        public void GivenScythe_WhenReadConfig_ThenReturnPath()
+        {
+            //Arrange
+            string path = Helper.Path;
+
+            var scythe = new Probe(path);
+            //Act
+            var m = Directory.Exists(path);
+
+            //Assert
+            Assert.IsTrue(Directory.Exists(path));
+        }
+
+        [TestMethod]
+        public void GivenScythe_WhenMarkerCalled_ThenProbesMarker()
+        {
+            //Arrange
+            string path = Helper.Path;
+            var scythe = new Probe(path);
+            string marker = "First_Marker";
+
+            //Act
+            Probe.SetMarkTime(marker, new DateTime(1984, 01, 01));
+
+            //Assert
+            Assert.IsTrue(Directory.Exists(path));
+        }
+
+    }
+
+    public static class Helper
+    {
+        public static string Path = @"D:\PlayGround\scythe\scythe\ScytheTest\bin\Debug\Probes\";
+    }
+}
